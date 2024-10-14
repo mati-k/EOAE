d872a0b399b9143b221d0e3b51f92fdba43687f4
mati <matik.github@gmail.com>
2024-10-14 16:50:09 +0200

    Initial setup

---
+++ SubModule.cs
@@ -0,0 +1,26 @@
+﻿using TaleWorlds.MountAndBlade;
+
+
+namespace Echoes_of_an_Empire___Code
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
