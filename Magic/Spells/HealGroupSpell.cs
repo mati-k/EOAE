f6ead790e66326071bd46597120e05ee054fe4fc
mati <matik.github@gmail.com>
2025-01-01 15:33:55 +0100

    feat: group heal spell

--- Magic/Spells/HealGroupSpell.cs
+++ Magic/Spells/HealGroupSpell.cs
@@ -4,22 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
 namespace EOAE_Code.Magic.Spells
 {
     public class HealGroupSpell : Spell
     {
+        public float HealEffect {  get; private set; }
+        public float HealRadius { get; private set; }
         public override bool IsThrown { get { return false; } }
 
         public HealGroupSpell(SpellDataXml data) : base(data)
         {
-
+            HealEffect = data.EffecValue;
+            HealRadius = data.Range;
         }
 
         public override void Cast(Agent caster)
         {
-            throw new NotImplementedException();
+            MBList<Agent> agents = new MBList<Agent>(Mission.Current.Agents);
+            Mission.Current.GetNearbyAllyAgents(caster.Position.AsVec2, HealRadius, caster.Team, agents);
+
+            foreach (Agent agent in agents)
+            {
+                agent.Health += Math.Min(agent.Health + HealEffect, agent.HealthLimit);
+            }
         }
     }
 }
