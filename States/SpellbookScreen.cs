b65df62ffbb1d146af878fe3b8f15a4a9ff30438
mati <matik.github@gmail.com>
2025-03-04 19:46:06 +0100

    feat: add spellbook button in character screen

---
+++ States/SpellbookScreen.cs
@@ -0,0 +1,76 @@
+﻿using EOAE_Code.Magic;
+using TaleWorlds.Core;
+using TaleWorlds.Engine;
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.InputSystem;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade.View.Screens;
+using TaleWorlds.ScreenSystem;
+
+namespace EOAE_Code.States
+{
+    [GameStateScreen(typeof(SpellbookState))]
+    public class SpellbookScreen : ScreenBase, IGameStateListener
+    {
+        private const int SPELL_SLOTS = 5;
+
+        private GauntletLayer gauntletLayer;
+        private SpellbookState state;
+        private MagicHudVM vm;
+
+        public SpellbookScreen(SpellbookState state)
+        {
+            this.state = state;
+            state.RegisterListener(this);
+        }
+
+        protected override void OnFrameTick(float dt)
+        {
+            base.OnFrameTick(dt);
+            LoadingWindow.DisableGlobalLoadingWindow();
+            if (
+                gauntletLayer.Input.IsHotKeyDownAndReleased("Exit")
+                || gauntletLayer.Input.IsGameKeyDownAndReleased(41)
+            )
+            {
+                Close();
+            }
+        }
+
+        void IGameStateListener.OnActivate()
+        {
+            base.OnActivate();
+
+            vm = new MagicHudVM();
+            gauntletLayer = new GauntletLayer(1, "GauntletLayer", true);
+            gauntletLayer.InputRestrictions.SetInputRestrictions(true, InputUsageMask.All);
+            gauntletLayer.Input.RegisterHotKeyCategory(
+                HotKeyManager.GetCategory("GenericCampaignPanelsGameKeyCategory")
+            );
+            gauntletLayer.LoadMovie("MagicHUD", vm);
+            gauntletLayer.IsFocusLayer = true;
+            base.AddLayer(gauntletLayer);
+            ScreenManager.TrySetFocus(gauntletLayer);
+        }
+
+        void IGameStateListener.OnDeactivate()
+        {
+            base.OnDeactivate();
+        }
+
+        void IGameStateListener.OnFinalize()
+        {
+            base.OnFinalize();
+        }
+
+        void IGameStateListener.OnInitialize()
+        {
+            base.OnInitialize();
+        }
+
+        private void Close()
+        {
+            Game.Current.GameStateManager.PopState();
+        }
+    }
+}
---
+++ States/SpellbookState.cs
@@ -0,0 +1,11 @@
+﻿using TaleWorlds.Core;
+
+namespace EOAE_Code.States
+{
+    public class SpellbookState : GameState
+    {
+        public override bool IsMenuState => true;
+
+        public SpellbookState() { }
+    }
+}
