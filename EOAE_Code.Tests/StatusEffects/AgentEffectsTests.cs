1443bb58f7f4b43d7f79d1c39ad02e4610148baa
mati <matik.github@gmail.com>
2025-06-17 20:50:36 +0200

    fix: make tests find assemblies

--- EOAE_Code.Tests/StatusEffects/AgentEffectsTests.cs
+++ EOAE_Code.Tests/StatusEffects/AgentEffectsTests.cs
@@ -1,8 +1,18 @@
+using EOAE_Code.Tests.Fixtures;
+using NSubstitute;
+using TaleWorlds.MountAndBlade;
+
 namespace EOAE_Code.Tests.StatusEffects
 {
     public class AgentEffectsTests
     {
         [Fact]
-        public void InstantiatesAgentEffects() { }
+        public void InstantiatesAgentEffects()
+        {
+            var mockAgent = Substitute.For<Agent>();
+            var agentEffects = new AgentEffectsFixture(mockAgent);
+
+            Assert.NotNull(agentEffects);
+        }
     }
 }
