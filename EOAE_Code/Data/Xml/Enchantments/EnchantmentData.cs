1b3207346d969b05e58c32760c3f7d8d6402694f
mati <matik.github@gmail.com>
2025-06-24 20:41:49 +0200

    feat add enchantment tooltip on item

--- EOAE_Code/Data/Xml/Enchantments/EnchantmentData.cs
+++ EOAE_Code/Data/Xml/Enchantments/EnchantmentData.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using EOAE_Code.Data.Xml.StatusEffects;
+using EOAE_Code.Extensions;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Inventory;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.Localization;
 
 namespace EOAE_Code.Data.Xml.Enchantments
@@ -11,32 +14,54 @@ namespace EOAE_Code.Data.Xml.Enchantments
     public class EnchantmentData
     {
         [XmlAttribute]
-        public string Name = "";
+        public string Name { get; set; } = "";
 
         [XmlAttribute]
-        public string DisplayName = "";
+        public string DisplayName { get; set; } = "";
 
         // ToDo: just temporary thing. Need to make some integrated way for status effects that would be for enchantments, spells, potion etc. with easy reuse and compatibility
         [XmlAttribute]
-        public string DescriptionKey = "";
+        public string DescriptionKey { get; set; } = "";
 
         // For now use preview from some items, in future maybe custom pictures
         [XmlAttribute]
-        public string IconItem = "";
+        public string IconItem { get; set; } = "";
 
         [XmlAttribute]
-        public bool UseSlider = false;
+        public bool UseSlider { get; set; } = false;
 
         [XmlArray("ItemTypes")]
         [XmlArrayItem("ItemType")]
-        public List<ItemObject.ItemTypeEnum> ItemTypes = new();
+        public List<ItemObject.ItemTypeEnum> ItemTypes { get; set; } = new();
 
         [XmlElement("StatusEffectTemplate")]
-        public StatusEffectTemplate StatusEffectTemplate = new StatusEffectTemplate();
+        public StatusEffectTemplate StatusEffectTemplate { get; set; } = new StatusEffectTemplate();
 
         public TextObject GetDescription(float value)
         {
             return new TextObject(DescriptionKey, null).SetTextVariable("value", value);
+        }
+
+        public void AddTooltips(ItemMenuVM itemMenuVM)
+        {
+            itemMenuVM.AddTooltip(
+                "",
+                new TextObject("{=aVhgwU2u}Enchantment:").ToString(),
+                Color.ConvertStringToColor("#4470F2FF")
+            );
+
+            itemMenuVM.AddTooltip("", new TextObject(DisplayName).ToString(), Color.Black);
+
+            if (!string.IsNullOrEmpty(DescriptionKey))
+            {
+                itemMenuVM.AddTooltip(
+                    "",
+                    new TextObject(DescriptionKey)
+                        .SetTextVariable("value", StatusEffectTemplate.Scale)
+                        .ToString(),
+                    Color.Black
+                );
+            }
         }
     }
 }
---
+++ EOAE_Code/Patches/TooltipPatch.cs
@@ -0,0 +1,35 @@
+﻿using EOAE_Code.Data.Managers;
+using EOAE_Code.Extensions;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Inventory;
+using TaleWorlds.Core;
+using TaleWorlds.Core.ViewModelCollection;
+using TaleWorlds.ObjectSystem;
+
+namespace EOAE_Code.Patches;
+
+[HarmonyPatch]
+public class TooltipPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ItemMenuVM), "SetItem")]
+    public static void AddSpecialTooltips(
+        SPItemVM item,
+        ItemVM comparedItem,
+        BasicCharacterObject character,
+        int alternativeUsageIndex,
+        ItemMenuVM __instance
+    )
+    {
+        if (BookManager.IsBook(item.StringId))
+        {
+            BookManager.GetBook(item.StringId).AddTooltips(__instance);
+        }
+
+        var itemObject = MBObjectManager.Instance.GetObject<ItemObject>(item.StringId);
+        if (itemObject.IsEnchanted())
+        {
+            itemObject.GetEnchantment()!.Enchantment.AddTooltips(__instance);
+        }
+    }
+}
