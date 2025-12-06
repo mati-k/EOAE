1890c3e04418afc67240b812052552b379afe939
mati <matik.github@gmail.com>
2025-12-06 19:59:37 +0100

    Change old TradeBoundPatch => use of the new model

---
+++ EOAE_Code/Models/CustomVillageTradeModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ComponentInterfaces;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.ModuleManager;
+
+namespace EOAE_Code.Models
+{
+    public class CustomVillageTradeModel : VillageTradeModel
+    {
+        private readonly float tradeBoundDistance;
+
+        public CustomVillageTradeModel()
+        {
+            tradeBoundDistance = Convert.ToSingle(
+                File.ReadAllText(
+                    ModuleHelper.GetModuleFullPath("EOAE_Code") + "TradeBoundDistance.txt"
+                )
+            );
+        }
+
+        public override Settlement GetTradeBoundToAssignForVillage(Village village)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override float TradeBoundDistanceLimitAsDays(
+            MobileParty.NavigationType navigationType
+        )
+        {
+            // Default implementation for now
+            return Campaign.Current.GetAverageDistanceBetweenClosestTwoTownsWithNavigationType(
+                    navigationType
+                )
+                * 3f
+                / (
+                    Campaign.Current.EstimatedAverageVillagerPartySpeed
+                    * (float)CampaignTime.HoursInDay
+                );
+        }
+    }
+}
--- EOAE_Code/SubModule.cs
+++ EOAE_Code/SubModule.cs
@@ -49,7 +49,6 @@ namespace EOAE_Code
             );
             XmlDataLoader.LoadXmlDataList<EnchantmentData, EnchantmentManager>("enchantments.xml");
 
-            TradeBoundPatch.Apply(Harmony);
             Harmony.PatchAll();
 
             var uiExtender = UIExtender.Create("EOAE_Code");
@@ -93,6 +92,7 @@ namespace EOAE_Code
             base.OnGameStart(game, gameStarterObject);
 
             gameStarterObject.AddModel(new AgentStatusEffectModel());
+            gameStarterObject.AddModel(new CustomVillageTradeModel());
         }
     }
 }
